2e4760fef6951400f9510f4544b69d1dc9ba2d19
Sux86 <156996986+Sux86@users.noreply.github.com>
2024-05-16 15:34:54 +0800

    Update solve_svm_qp.m

--- solve_svm_qp.m
+++ solve_svm_qp.m
@@ -3,6 +3,6 @@ function [x2,fval2] = solve_svm_qp(X,H,delta,C)
     Q=(X*(H+I)*X').*delta;
     e=ones(size(delta,1),1);
     options.tmax='10000';
-    [x2,fval2] = qpbsvm(double(Q),-e,C,[],[],options);
+    [x2,fval2] = bsvm(double(Q),-e,C,[],[],options);
 end
 
