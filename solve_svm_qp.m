24fbc56d490533213c0c9bd936a22d8f408ed1f5
Sux86 <156996986+Sux86@users.noreply.github.com>
2024-05-16 13:48:30 +0800

    Update solve_svm_qp.m

--- solve_svm_qp.m
+++ solve_svm_qp.m
@@ -4,6 +4,5 @@ function [x2,fval2] = solve_svm_qp(X,H,delta,C)
     e=ones(size(delta,1),1);
     options.tmax='10000';
     [x2,fval2] = qpbsvm(double(Q),-e,C,[],[],options);
-    % [x2,fval2] = quadprog(double(Q),-e,[],[],[],[],0*e,C*e);
 end
 
