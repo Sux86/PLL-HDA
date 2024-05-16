ca7eeb69497e6bc7d7e0068e1e394c8a6e51e5f9
Sux86 <156996986+Sux86@users.noreply.github.com>
2024-05-16 18:35:57 +0800

    Delete solve_svm_qp.m

--- solve_svm_qp.m
+++
@@ -1,8 +0,0 @@
-function [x2,fval2] = solve_svm_qp(X,H,delta,C)
-    I=eye(size(H,1));
-    Q=(X*(H+I)*X').*delta;
-    e=ones(size(delta,1),1);
-    options.tmax='10000';
-    [x2,fval2] = bsvm(double(Q),-e,C,[],[],options);
-end
-
